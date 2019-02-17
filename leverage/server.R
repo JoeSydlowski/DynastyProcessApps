2cdd52fbfb39dad5a973e1e9ed83a517b40c91cd
Joe Sydlowski <syd235@gmail.com>
2019-02-17 12:40:23 -0600

    Read csv error

--- leverage/server.R
+++ leverage/server.R
@@ -4,10 +4,8 @@ library(shinythemes)
 library(ggplot2)
 library(plotly)
 
-#x <- read.csv(curl("https://raw.githubusercontent.com/tanho63/dynastyprocess/master/files/database.csv"))
-#cols <- c(15:18,56:62)
-
-x <- read.csv(curl("https://raw.githubusercontent.com/tanho63/dynastyprocess/master/files/fp_dynastyvsredraft.csv"))
+x <- read.csv(curl("https://raw.githubusercontent.com/tanho63/dynastyprocess/master/files/fp_dynastyvsredraft.csv"),
+              encoding = "unknown")
 
 dates <- tail(unique(x$date),3)
 
@@ -15,7 +13,7 @@ shinyServer(function(input, output) {
   
   output$distPlot <- renderPlotly({
     ggplotly(ggplot(x[(x$pos == input$posFilter) &
-               (x$date %in% dates),], aes(dynpECR, rdpECR, group=X.U.FEFF.name)) + 
+               (x$date %in% dates),], aes(dynpECR, rdpECR, group=name)) + 
       geom_point(aes(color=date), size = 2) +
       geom_point(shape = 1, color = "black", size = 2) +
       geom_line() +
