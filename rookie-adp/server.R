beb52361ae8d316dbfcb2463fccd7fdd0e38a2f6
Joe Sydlowski <syd235@gmail.com>
2019-05-17 22:15:16 -0500

    Surprise!

---
+++ rookie-adp/server.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(curl)
+library(DT)
+library(dplyr)
+library(anytime)
+
+x <- read.csv(curl("https://raw.githubusercontent.com/JoeSydlowski/DynastyProcess/master/rookie-adp/adp-picks.csv"))
+x$pick_timestamp <- ifelse(x$MFL_Sleeper == "Sleeper", x$pick_timestamp/1000, x$pick_timestamp)
+x$date <- anydate(x$pick_timestamp)
+min(x$date)
+
+shinyServer(function(input, output) {
+  QBs <- reactive({
+    if(substring(input$mode, 1, 1) == "1")
+      {"1QB"}
+    else if(substring(input$mode, 1, 1) == "2")
+      {"2QB/SF"}
+  })
+  
+  OffDef <- reactive({
+    if(substring(input$mode, 5, 5) == "I")
+      {c("IDP","Offense-only")}
+    else if (substring(input$mode, 5, 5) == "O")
+      {"Offense-only"}
+  })
+  
+  
+  df <- reactive({
+    df1 <- x %>%
+      filter(QB_type == QBs() &
+               IDP %in% OffDef() &
+               date <= input$dateRange[2] &
+               date >= input$dateRange[1]) %>%
+      group_by(name, pos, tm, college) %>%
+      summarise(ADP = round(mean(pick),1),
+                Count = n()) %>%
+      arrange(ADP)
+
+    print(input$dateRange[1])
+    
+    df1
+  })  
+
+  output$results <- renderDT({
+    datatable( df(),
+               filter = 'top',
+               options = list(pageLength = 50,
+                              scrollX =TRUE,
+                              columnDefs = list(list(className = 'dt-head-left', targets = "_all"))),
+               class = 'compact stripe')
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {"DynastyProcessRookieADP.csv"},
+    content = function(file) {write.csv(df(), file)}
+  )
+  
+  proxy = dataTableProxy('results')
+  
+  observeEvent(input$clear1, {proxy %>% clearSearch()}
+  )
+  
+})
---
+++ rookie-adp/ui.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(curl)
+library(DT)
+library(dplyr)
+library(anytime)
+
+x <- read.csv(curl("https://raw.githubusercontent.com/JoeSydlowski/DynastyProcess/master/rookie-adp/adp-picks.csv"))
+x$pick_timestamp <- ifelse(x$MFL_Sleeper == "Sleeper", x$pick_timestamp/1000, x$pick_timestamp)
+x$date <- anydate(x$pick_timestamp)
+
+shinyUI(fluidPage(
+  theme = shinytheme("flatly"),
+  withTags(
+    nav(class="navbar navbar-default navbar-static-top", role="navigation",
+        div(class="container-fluid",
+            div(class="navbar-header",
+                span(class="navbar-brand",
+                     a(href="https://dynastyprocess.com",strong("DynastyProcess.com"))
+                )
+            ),
+            ul(class="nav navbar-nav",
+               li(class="active",a(href="#",strong("Database"))
+               ),
+               li(
+                 a(href="http://apps.dynastyprocess.com/calculator",strong("Calculator"))
+               ),
+               li(
+                 a(href="http://apps.dynastyprocess.com/rookie-adp",strong("Rookie ADP"))
+               ),
+               li(class="dropdown",
+                  a(class="dropdown-toggle",`data-toggle`="dropdown", `data-value`="More Awesome Apps",`aria-expanded`="false", href="#", strong("More Awesome Apps"),b(class="caret")),
+                  ul(class="dropdown-menu",
+                     li(a(href="http://apps.dynastyprocess.com/arbitrage",strong("Arbitrage"))),
+                     li(a(href="http://apps.dynastyprocess.com/ecr",strong("ECR Explorer"))),
+                     li(a(href="http://apps.dynastyprocess.com/cohort",strong("Cohort")))
+                  )
+               )
+            )
+        )
+    )
+  ),
+  
+  titlePanel("DynastyProcess.com Rookie ADP"),
+  fluidRow(
+    column(4,
+           radioButtons("mode",
+                        "Select Mode",
+                        choices = list("1QB_O" = "1QB_O",
+                                       "2QB_O" = "2QB_O",
+                                       "1QB_IDP" = "1QB_IDP",
+                                       "2QB_IDP" = "2QB_IDP"),
+                        selected = "1QB_O")),
+    column(4,
+           dateRangeInput("dateRange",
+                          "Date Range",
+                          start = min(x$date),
+                          end = max(x$date),
+                          min = min(x$date),
+                          max = max(x$date))),
+    column(4,
+            actionButton("clear1", "Reset All Filters"),
+            downloadButton("downloadData", "Download"))),
+  hr(),
+  DTOutput("results"),
+  hr(),
+  p(HTML(paste0("This app was created by ",
+                a(href = "https://twitter.com/JoeSydlowskiFF", "Joe Sydlowski"),
+                " based on data from ",
+                a(href = "https://dynastyprocess.com/database", "DynastyProcess.com"),
+                ". You can find the code at ",
+                a(href = "https://github.com/JoeSydlowski/DynastyProcess/tree/master/database", "my github"),
+                "."
+  )))
+))
