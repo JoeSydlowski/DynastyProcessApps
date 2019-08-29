930f4d9cc08248b74cac915c41387f403c8d0a94
root <root@ip-172-31-9-82.us-east-2.compute.internal>
2019-08-29 22:23:21 +0000

    server changes

---
+++ scripts/ECRCombine.R
@@ -0,0 +1,211 @@
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(purrr)
+
+#Sys.setenv(TZ="CST")
+#todayMonth <- month(today())
+#todayDay <- day(today())
+
+#teamIDs <- read.csv("C:/Users/syd23/OneDrive/Documents/DynastyProcess/database-v2/teamIDs.csv")
+
+read_plus <- function(flnm) {
+  #%>%  mutate(filename = flnm)
+  
+  if(grepl('dynasty-overall|rookies',flnm))
+  {vecNames <- c('Rank','a1','Player','Pos','Bye','Age','Best','Worst','Avg','SD','Notes')}
+  else if(grepl('dst|cheatsheets',flnm))
+  {vecNames <- c('Rank','a1','Player','Bye','Best','Worst','Avg','SD','Notes')}
+  #else if(grepl('cheatsheets',flnm))
+  #{names(df) <- c('Rank','a1','Player','Bye','Best','Worst','Avg','SD','Notes')}
+  else
+  {vecNames <- c('Rank','a1','Player','Bye','Age','Best','Worst','Avg','SD','Notes')}
+  
+  df <- read.csv(flnm, col.names = vecNames, sep = ',') 
+  
+  
+  numCols <- c("Rank","Bye","Best","Worst","Avg","SD")#,"ADP","vs, ADP")
+  df[numCols] <- lapply(df[numCols], as.numeric)
+  df$Filename <- flnm
+  df
+}
+
+tbl_with_sources <-
+  list.files(pattern = "*.csv", 
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+setwd("/srv/data/files/fp-weekly-scrapes-data")
+
+temp <- list.files(pattern="dst")
+myfiles <- lapply(temp, read.delim)
+
+
+
+if (between(todayMonth,10,12) | (todayMonth == 9 & todayDay >= 10))
+{list_of_pages <- c("dynasty-overall", "dynasty-qb", "dynasty-rb", "dynasty-wr",
+                    "dynasty-te", "dynasty-k", "dynasty-dst", "rookies",
+                    "ros-ppr-overall", "ros-qb", "ros-ppr-rb",
+                    "ros-ppr-wr", "ros-ppr-te", "ros-k", "ros-dst",
+                    "ros-idp", "ros-dl", "ros-lb", "ros-db",
+                    "ppr-flex", "qb", "ppr-rb", "ppr-wr", "ppr-te", "k", "dst", "idp", "dl", "lb", "db")
+} else if (between(todayMonth,9,12) | (todayMonth == 8 & todayDay >= 15))
+{list_of_pages <- c("dynasty-overall", "dynasty-qb", "dynasty-rb", "dynasty-wr",
+                    "dynasty-te", "dynasty-k", "dynasty-dst", "rookies",
+                    "ppr-cheatsheets", "qb-cheatsheets", "ppr-rb-cheatsheets",
+                    "ppr-wr-cheatsheets", "ppr-te-cheatsheets", "k-cheatsheets", "dst-cheatsheets",
+                    "idp-cheatsheets", "dl-cheatsheets", "lb-cheatsheets", "db-cheatsheets",
+                    "ppr-flex", "qb", "ppr-rb", "ppr-wr", "ppr-te", "k", "dst", "idp", "dl", "lb", "db")
+
+} else
+{list_of_pages <- c("dynasty-overall", "dynasty-qb", "dynasty-rb", "dynasty-wr",
+                    "dynasty-te", "dynasty-k", "dynasty-dst", "rookies",
+                    "ppr-cheatsheets", "qb-cheatsheets", "ppr-rb-cheatsheets",
+                    "ppr-wr-cheatsheets", "ppr-te-cheatsheets", "k-cheatsheets", "dst-cheatsheets",
+                    "idp-cheatsheets", "dl-cheatsheets", "lb-cheatsheets", "db-cheatsheets")
+}
+
+
+
+dfComb <- data.frame()
+
+for (page in list_of_pages)
+{
+  url <- paste0("https://www.fantasypros.com/nfl/rankings/",page,".php")
+  webpage <- read_html(url)
+  df <- webpage %>%
+    html_node("table") %>%
+    html_table(fill=TRUE) %>%
+    .[4:min(10,ncol(.))]
+  
+  df2 <- webpage %>%
+    html_node("table") %>%
+    html_nodes(".player-row")
+  
+  dfName <- data.frame(
+    Player = html_text(html_node(df2,'.full-name')),
+    Team = html_text(html_node(df2,'.grey'))
+  )
+  
+  df <- df[!(df$Avg == "" | df$Avg == "&nbsp" | grepl('googletag', df$Avg)), ]
+  
+  #numCols <- c("Rank","Bye","Best","Worst","Avg","Std Dev","ADP","vs, ADP")
+  if(page %in% c('dynasty-overall', 'rookies', 'ppr-cheatsheets', 'ros-ppr-overall', 'ppr-flex', 'idp', 'idp-cheatsheets', 'ros-idp'))
+  {
+    numCols <- c(4:9)
+  } else 
+  {numCols <- c(3:8)}
+  
+  if(nrow(dfName) > 0) {
+    df <- cbind(dfName, df)
+    df$Page <- page
+  } else
+  {df <- data.frame()
+  numCols <- c()}
+  
+  df[numCols] <- lapply(df[numCols], as.numeric)
+  
+  if(!is.null(df$Pos))
+  {df$Pos <- substr(df$Pos,1,2)}
+  
+  if(nrow(df) > 0)
+  {
+    
+    if (grepl("qb",page))
+    {df$Pos <- "QB"}
+    else if (grepl("rb", page))
+    {df$Pos <- "RB"}
+    else if (grepl("wr", page))
+    {df$Pos <- "WR"}
+    else if (grepl("te", page))
+    {df$Pos <- "TE"}
+    else if (grepl("-k", page))
+    {df$Pos <- "K"}
+    else if (grepl("k-", page))
+    {df$Pos <- "K"}
+    else if (page == "k")
+    {df$Pos <- "K"}
+    else if (grepl("dst", page))
+    {df$Pos <- "DST"}
+    else if (grepl("dl", page))
+    {df$Pos <- "DL"}
+    else if (grepl("lb", page))
+    {df$Pos <- "LB"}
+    else if (grepl("db", page))
+    {df$Pos <- "DB"}
+    
+    df$Pos[df$Pos == "DS"] <- "DST"
+    df$Pos[df$Pos == "CB"] <- "DB"
+    df$Pos[df$Pos == "DE"] <- "DL"
+    df$Pos[df$Pos == "DT"] <- "DL"
+    
+    df$Pos <- gsub('(^S[0-9]*$)',"DB",df$Pos)
+    df[grepl("K", df$Pos), 'Pos'] <- 'K'
+    
+    dfComb <- bind_rows(dfComb, df)
+  }
+  
+}
+
+colnames(dfComb)[colnames(dfComb)=="Std Dev"] <- "SD"
+colnames(dfComb)[colnames(dfComb)=="Avg"] <- "ECR"
+
+dfComb$Page[dfComb$Page == "dynasty-overall"] <- "do"
+dfComb$Page[dfComb$Page %in% c("ppr-cheatsheets", "ros-ppr-overall", 'idp-cheatsheets', 'ros-idp' )] <- "ro"
+
+dfComb$Page[dfComb$Page %in% c("ppr-flex","idp")] <- "wo"
+
+dfComb$Page[dfComb$Page == "rookies"] <- "rook"
+
+dfComb$Page[grep("dynasty", dfComb$Page)] <- "dp"
+dfComb$Page[grep("cheatsheets", dfComb$Page)] <- "rp"
+dfComb$Page[grep("ros", dfComb$Page)] <- "rp"
+dfComb$Page[dfComb$Page %in% c("qb", "ppr-rb", "ppr-wr", "ppr-te", "k", "dst", "idp", "dl", "lb", "db")] <- "wp"
+
+
+dfTotal <- dfComb[1:10] %>%
+  select(Player, Team, Pos, ECR, Best, Worst, Page, SD) %>%
+  group_by(Player) %>%
+  gather(variable, value, ECR, SD, Best, Worst) %>%
+  unite(temp, Page, variable, sep = '') %>%
+  spread(temp, value)
+
+dfTotal$Player <- gsub('(-)|[[:punct:]]|( Jr)|( Sr)|( III)|( II)|( IV)','\\1',dfTotal$Player)
+
+dfTotal$ScrapeDate <- Sys.Date()
+
+dstSplit <- dfTotal %>%
+  filter(Pos == "DST") %>%
+  separate(Player, c("Player", "Team"), '[ ](?=[^ ]+$)')
+
+dfTotal <- dfTotal %>%
+  filter(Pos != "DST")
+
+dfTotal <- bind_rows(dfTotal, dstSplit)
+
+dfTotal <- merge(dfTotal, teamIDs, by.x='Team', by.y='spotrac', all.x = TRUE )
+
+dfTotal$Team <- dfTotal$mfl
+dfTotal[c("X","mfl")] <- NULL
+
+dfTotal <- dfTotal %>%
+  select(Player,  everything()) %>%
+  arrange(roECR)
+
+write.csv(dfTotal,
+          file = paste0("FPScrape-", format(Sys.Date(), format = "%Y%m%d"),".csv"),
+          row.names=FALSE,
+          na="")
+
+library(ggplot2)
+library(ggrepel)
+
+
+temp <- dfTotal %>%
+  filter(Pos == "TE" & wpECR < 30)
+
+ggplot(temp, aes(wpECR, rpECR)) +
+  geom_point() + 
+  geom_abline() +
+  geom_text_repel(label = temp$Player)
