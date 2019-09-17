80150b4d1799617f7ce48e4af04e79440df5bb8c
Tan Ho <tan@tanho.ca>
2019-09-16 22:15:03 -0400

    BABY'S FIRST SHINY APP WOOOO

--- crystalball/app.R
+++ crystalball/app.R
@@ -7,60 +7,54 @@ library(lubridate)
 library(shiny)
 
 ui <- fluidPage(
-  textInput(franchiseid,"MFL League ID",value="54040",placeholder="Should be five digits!"),
+  textInput('franchiseid',"MFL League ID",value="54040",placeholder="Should be five digits!"),
   fluidRow(DTOutput('summarytbl'))
   )
 
 server <- function(input, output, session) {
   
-  reactive({
-    
-  franchises<-fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=league&L=",reactive(input$franchiseid),"&APIKEY=&JSON=1"))$league$franchises$franchise %>%
-    select(ownerid=id,owner=name)
+  franchises<-reactive({fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=league&L=",input$franchiseid,"&APIKEY=&JSON=1"))$league$franchises$franchise %>%
+    select(ownerid=id,owner=name)})
   
-  standings<-fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=leagueStandings&L=",reactive(input$franchiseid),"&APIKEY=&JSON=1"))$leagueStandings$franchise %>% 
+  standings<-reactive({fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=leagueStandings&L=",input$franchiseid,"&APIKEY=&JSON=1"))$leagueStandings$franchise %>% 
     select(ownerid=id,pointsfor=pf,potentialpoints=pp,starts_with('h2h'),starts_with('all_play')) %>%
     mutate_at(vars(starts_with('all_play')),as.numeric) %>%
     mutate(record=paste0(h2hw,"-",h2ht,"-",h2hl),
            allplaypct=all_play_w/(all_play_w+all_play_t+all_play_l)) %>%
     select(-starts_with('all_play')) %>%
-    inner_join(franchises)
+    inner_join(franchises())})
     
   
-  schedule <-fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=schedule&L=",reactive(input$franchiseid),"&APIKEY=&JSON=1"))$schedule$weeklySchedule %>% 
+  schedule <-reactive({fromJSON(paste0("https://www03.myfantasyleague.com/2019/export?TYPE=schedule&L=",input$franchiseid,"&APIKEY=&JSON=1"))$schedule$weeklySchedule %>% 
     unnest(matchup) %>% 
     unnest_wider(franchise) %>%
     filter(result=='NA') %>% 
     select(id,week) %>%
-    hoist(id,team=1,opp=2)
+    hoist(id,team=1,opp=2)})
   
-})
-  
-  fullschedule <- schedule %>%
+  fullschedule <- reactive({schedule() %>%
     select(week,team=opp,opp=team) %>%
-    bind_rows(schedule) %>%
+    bind_rows(schedule()) %>%
     mutate(week=as.numeric(week)) %>% 
     arrange(week,team) %>%
-    nest_join(standings, by=c("team"="ownerid")) %>%
-    rename(teaminfo=standings) %>%
-    nest_join(standings,by=c("opp"="ownerid")) %>%
-    rename(oppinfo=standings) %>%
+    nest_join(standings(), by=c("team"="ownerid"),name='teaminfo') %>%
+    nest_join(standings(),by=c("opp"="ownerid"),name='oppinfo') %>%
     hoist(teaminfo,team_name="owner",team_pct="allplaypct") %>%
     hoist(oppinfo,opp_name="owner",opp_pct="allplaypct") %>%
     select(-teaminfo,-oppinfo,-opp) %>%
-    mutate(win_prob=round(team_pct/(team_pct+opp_pct),digits=3))
+    mutate(win_prob=round(team_pct/(team_pct+opp_pct),digits=3))})
   
-  expectedwins<-fullschedule %>%
+  expectedwins<-reactive({fullschedule() %>%
     group_by(team,team_name) %>%
     summarize(ewins=sum(win_prob,na.rm=TRUE),
               elosses=n()-ewins) %>%
     ungroup() %>%
-    nest_join(standings,by=c('team'='ownerid')) %>%
+    nest_join(standings(),by=c('team'='ownerid'),name='standings') %>%
     hoist(standings,h2hw="h2hw",h2ht="h2ht",h2hl="h2hl") %>%
     mutate_at(vars("h2hw","h2ht","h2hl"),as.numeric) %>%
     mutate(total_wins=ewins+h2hw, total_losses=elosses+h2hl) %>%
     select(team,team_name,wins=h2hw,losses=h2hl,ewins,elosses,total_wins,total_losses) %>%
-    arrange(desc(total_wins))
+    arrange(desc(total_wins))})
   
 
   
@@ -71,7 +65,7 @@ server <- function(input, output, session) {
     return(breakvalue)
   }
   
-  output$summarytbl<- renderDT(expectedwins)
+  output$summarytbl<- renderDT(expectedwins())
   
 
 }
